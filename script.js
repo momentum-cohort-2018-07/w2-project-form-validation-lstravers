e8f5a21d86c2350ecc7fac0bcc4202f1b94fe1d7
lstravers <stravers222@gmail.com>
2018-08-09 16:02:06 -0400

    modified form and script files to make one working function

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log('Add validation!');
+
+document.getElementById('parking-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var name = document.getElementById('name').value.trim()
+
+    if (name === "") {
+        showEmptyFieldError()
+    }
+}
+)
+    function showEmptyFieldError() {
+        var errorDiv = document.createElement('div')
+       errorDiv.classList.add('input-invalid') 
+        var field = document.getElementById('name-field')
+       // field.parentElement.appendChild(errorDiv)
+       field.classList.add('input-invalid')
+    }   
+
+
+
+   // clearError()
+// innerhtml
+
+/*
+  function clearError () {
+    var errorMsg = field.parentElement.querySelector('.error-msg') 
+    if (errorMsg) {
+      errorMsg.remove()
+    }
+  }
+  */
