3415fe92ea843fb3ed3f1eb4e571aac8e7e9d2b7
lstravers <stravers222@gmail.com>
2018-08-10 16:46:58 -0400

    modified script.js file.

--- script.js
+++ script.js
@@ -4,16 +4,36 @@ document.getElementById('parking-form').addEventListener('submit', function (eve
     event.preventDefault()
 
     var field = document.getElementById('name').value.trim()
+    // var car
+    startDateField = document.getElementById('start-date').value.trim()
+    //daysField = document.getElementById('days').value.trim()
+    //creditCardField = document.getElementById('credit-card').value.trim()
+    //cvvField = document.getElementById('cvv').value.trim()
+    //expirationField = document.getElementById('expiration').value.trim()
 
-    if (field === "") {
+    if (field === "") {                       //NAME FIELD
         showEmptyNameFieldError()
     }
-
-    var daysField = document.getElementById('days-field').value.trim()
+/*
+    if (startDateField === "") {             //START DATE FIELD
+        showEmptyStartDateFieldError()
+    }
 
     if (daysField === "") {
-        showEmptyDaysFieldError()
+        show
     }
+    if (creditCardField === "") {
+        show
+    }
+
+    if (cvvField === "") {
+
+    }
+
+    if (expirationField === "") {
+
+    }
+*/
 })
 //NAME FIELD
 function showEmptyNameFieldError() {
@@ -25,43 +45,18 @@ function showEmptyNameFieldError() {
 //CAR MAKE-MODEL-FIELD
 
 //DATE PARKING
-
-//NUMBER OF DAYS
-function showEmptyDaysFieldError() {
+function showEmptyStartDateFieldError() {
     var errorDiv = document.createElement('div')
     errorDiv.classList.add('input-invalid')
-    var field = document.getElementById('days-field')
-    field.classList.add('input-invalid')
+    var startDateField = document.getElementById('start-date')
+    startDateField.classList.add('input-invalid')
 }
+
+//NUMBER OF DAYS
+
 
 //CREDIT CARD
 
 //CVV
 
 //EXPIRATION
-
-
-/*
-document.getElementById('parking-form').addEventListener('submit', function (event) {
-    var inputElements = document.getElementsByTagName ('input')
-    for (var input of inputElements) {
-        if(input.value.trim() === ('')
-}
-
-*/
-//function showError(){
-// use forEach on the node list to loop througth the array
-// field.parentElement.appendChild(errorDiv)
-
-
-   // clearError()
-// innerhtml
-//querySelectorAll
-/*
-  function clearError () {
-    var errorMsg = field.parentElement.querySelector('.error-msg') 
-    if (errorMsg) {
-      errorMsg.remove()
-    }
-  }
-  */
